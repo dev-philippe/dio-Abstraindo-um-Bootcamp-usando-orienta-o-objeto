500d86c49e0843434f147992177d038688a7f768
unknown <philippediniz@outlook.com>
2024-05-23 09:59:10 -0300

    Herança e polimorfismo Parte 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -17,6 +18,7 @@ public class Main {
         curso2.setDescricao("Descrição Curso JS");
         curso2.setCargaHoraria(4);
 
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria java");
         mentoria.setDescricao("Descrição Mentoria Java");
@@ -25,7 +27,6 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
-
 
 
     }
